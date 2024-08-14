b4f5c28171c3dd6c4e0dad2b2b2e97e67c16d45b
jaimesmarco21 <153144300+jaimesmarco21@users.noreply.github.com>
2024-08-14 17:56:49 -0500

    Program

---
+++ Program/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+static void Main()
+{ 
+       // Mostrar un título en la consola
+Console.WriteLine("Bienvenido a mi primera aplicación en C#!");
+// Solicitar al usuario su nombre
+Console.Write("Por favor, ingrese su nombre:");
+string nombre = Console.ReadLine();
+// Mostrar un saludo personalizado
+Console.WriteLine($"Hola, {nombre}! Gracias por usar esta aplicación.");
+// Esperar a que el usuario presione una tecla para cerrarla aplicación
+Console.WriteLine("Presione cualquier tecla para salir.");
+Console.ReadKey();
+}
+}
